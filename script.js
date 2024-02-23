22232923f6d8fb67509b12f84c8ec9f6994ad246
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2024-02-23 14:43:37 -0300

    Atualização do script

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-http://ip-api.com/json/
+const inputIp = document.getElementById("ip-input");
+const btn = document.querySelector(".btn");
+
+const getAddres = async () => {
+  const ipNumber = inputIp.value;
+  const apiUrl = `http://ip-api.com/json/${ipNumber}`;
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+
+  console.log(data);
+};
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  getAddres();
+});
