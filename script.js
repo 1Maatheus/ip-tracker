d00dc977bfd45f4f9fd9c5b7094b4997d16fa1b6
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2024-02-24 12:40:44 -0300

    Adicionado seção com infos da api

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 const inputIp = document.getElementById("ip-input");
 const btn = document.querySelector(".btn");
+const ipE = document.getElementById("ip");
+const locationE = document.getElementById("location");
+const timezoneE = document.getElementById("timezone");
+const ispE = document.getElementById("isp");
+const mainE = document.querySelector(".main");
 
-const getAddres = async () => {
+const getAddress = async () => {
   const ipNumber = inputIp.value;
   const apiUrl = `http://ip-api.com/json/${ipNumber}`;
   const response = await fetch(apiUrl);
   const data = await response.json();
   inputIp.value = "";
+
+  mainE.classList.add("slideIn");
+
+  ipE.textContent = data.query;
+  locationE.textContent = `${data.city}, ${data.region}`;
+  timezoneE.textContent = `${data.timezone
+    .replace("/", " - ")
+    .replace("_", " ")}`;
+  ispE.textContent = data.isp;
+
   console.log(data);
 };
 
 btn.addEventListener("click", (e) => {
   e.preventDefault();
-  getAddres();
+  getAddress();
 });
